db0fdd8b7f0dd98dedeab1816542b52e3b316d08
ZengLiyuan <brainburster@outlook.com>
2021-01-15 15:36:14 +0800

    refactor : MacroPlus.hpp

--- MacroPlus.hpp
+++ MacroPlus.hpp
@@ -5,7 +5,7 @@
 #pragma region Macro$overload
 #endif
 
-#define ARG_N( \
+#define $arg_n( \
       _0, _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
      _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
      _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
@@ -13,7 +13,7 @@
      _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
      _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
      _61,_62,_63,N,...) N
-#define RSEQ_N() \
+#define $rseq_n() \
      63,62,61,60,                   \
      59,58,57,56,55,54,53,52,51,50, \
      49,48,47,46,45,44,43,42,41,40, \
@@ -22,14 +22,14 @@
      19,18,17,16,15,14,13,12,11,10, \
      9,8,7,6,5,4,3,2,1,0
 
-#define FIXBUG(x) x //to fix msvc's bug
-#define ZERO_ARG(...) = _0, ##__VA_ARGS__
-#define NARG_I(...) FIXBUG(ARG_N(__VA_ARGS__))
-#define NARG(...) NARG_I(ZERO_ARG(__VA_ARGS__), RSEQ_N())
+#define $fixbug(x) x //for fixing msvc's bug
+#define $zero_arg(...) = _0, ##__VA_ARGS__
+#define $narg_i(...) $fixbug($arg_n(__VA_ARGS__))
+#define $narg(...) $narg_i($zero_arg(__VA_ARGS__), $rseq_n())
 
-#define CAT_(name, n) name##n
-#define CAT(name, n) CAT_(name, n)
-#define $overload(func, ...)  FIXBUG(CAT(func, NARG(__VA_ARGS__))(__VA_ARGS__))
+#define $_cat(name, n) name##n
+#define $cat(name, n) $_cat(name, n)
+#define $overload(func, ...)  $fixbug($cat(func, $narg(__VA_ARGS__))(__VA_ARGS__))
 
 #ifndef __GNUC__
 #pragma endregion
@@ -39,16 +39,16 @@
 #pragma region Normalize
 #endif
 
-#define A_COMMA_B(a, b, ...) FIXBUG(CAT(CAT(A_COMMA_B_, NARG(a)), NARG(b))(a, b)), ##__VA_ARGS__
-#define A_COMMA_B_00(a, b)
-#define A_COMMA_B_01(a, b) b
-#define A_COMMA_B_10(a, b) a
-#define A_COMMA_B_11(a, b) a, b
+#define $a_comma_b(a, b, ...) $fixbug($cat($cat($a_comma_b_, $narg(a)), $narg(b))(a, b)), ##__VA_ARGS__
+#define $a_comma_b_00(a, b)
+#define $a_comma_b_01(a, b) b
+#define $a_comma_b_10(a, b) a
+#define $a_comma_b_11(a, b) a, b
 
 #define $normalize(...) $overload($normalize_, __VA_ARGS__)
 #define $normalize_0()
 #define $normalize_1(a) a
-#define $normalize_2(...) FIXBUG(A_COMMA_B(__VA_ARGS__))
+#define $normalize_2(...) $fixbug($a_comma_b(__VA_ARGS__))
 #define $normalize_3(a, ...) $normalize_2(a, $normalize_2(__VA_ARGS__))
 #define $normalize_4(a, b, c, ...) $normalize_2(a, $normalize_3(b, c, __VA_ARGS__))
 #define $normalize_5(a, b, c, ...) $normalize_2(a, $normalize_4(b, c, __VA_ARGS__))
@@ -92,35 +92,35 @@
 #define $reduce_1(op)
 #define $reduce_2(op, a)      (a)
 #define $reduce_3(op, a, b)   (a) op (b)
-#define $reduce_4(op, a, ...) FIXBUG((a) op $reduce_3(op, __VA_ARGS__))
-#define $reduce_5(op, a, ...) FIXBUG((a) op $reduce_4(op, __VA_ARGS__))
-#define $reduce_6(op, a, ...) FIXBUG((a) op $reduce_5(op, __VA_ARGS__))
-#define $reduce_7(op, a, ...) FIXBUG((a) op $reduce_6(op,  __VA_ARGS__))
-#define $reduce_8(op, a, ...) FIXBUG((a) op $reduce_7(op,  __VA_ARGS__))
-#define $reduce_9(op, a, ...) FIXBUG((a) op $reduce_8(op,  __VA_ARGS__))
-#define $reduce_10(op, a, ...) FIXBUG((a) op $reduce_9(op,  __VA_ARGS__))
-#define $reduce_11(op, a, ...) FIXBUG((a) op $reduce_10(op,  __VA_ARGS__))
-#define $reduce_12(op, a, ...) FIXBUG((a) op $reduce_11(op,  __VA_ARGS__))
-#define $reduce_13(op, a, ...) FIXBUG((a) op $reduce_12(op,  __VA_ARGS__))
-#define $reduce_14(op, a, ...) FIXBUG((a) op $reduce_13(op,  __VA_ARGS__))
-#define $reduce_15(op, a, ...) FIXBUG((a) op $reduce_14(op,  __VA_ARGS__))
-#define $reduce_16(op, a, ...) FIXBUG((a) op $reduce_15(op,  __VA_ARGS__))
-#define $reduce_17(op, a, ...) FIXBUG((a) op $reduce_16(op,  __VA_ARGS__))
-#define $reduce_18(op, a, ...) FIXBUG((a) op $reduce_17(op,  __VA_ARGS__))
-#define $reduce_19(op, a, ...) FIXBUG((a) op $reduce_18(op,  __VA_ARGS__))
-#define $reduce_20(op, a, ...) FIXBUG((a) op $reduce_19(op,  __VA_ARGS__))
-#define $reduce_21(op, a, ...) FIXBUG((a) op $reduce_20(op,  __VA_ARGS__))
-#define $reduce_22(op, a, ...) FIXBUG((a) op $reduce_21(op,  __VA_ARGS__))
-#define $reduce_23(op, a, ...) FIXBUG((a) op $reduce_22(op,  __VA_ARGS__))
-#define $reduce_24(op, a, ...) FIXBUG((a) op $reduce_23(op,  __VA_ARGS__))
-#define $reduce_25(op, a, ...) FIXBUG((a) op $reduce_24(op,  __VA_ARGS__))
-#define $reduce_26(op, a, ...) FIXBUG((a) op $reduce_25(op,  __VA_ARGS__))
-#define $reduce_27(op, a, ...) FIXBUG((a) op $reduce_26(op,  __VA_ARGS__))
-#define $reduce_28(op, a, ...) FIXBUG((a) op $reduce_27(op,  __VA_ARGS__))
-#define $reduce_29(op, a, ...) FIXBUG((a) op $reduce_28(op,  __VA_ARGS__))
-#define $reduce_30(op, a, ...) FIXBUG((a) op $reduce_29(op,  __VA_ARGS__))
-#define $reduce_31(op, a, ...) FIXBUG((a) op $reduce_30(op,  __VA_ARGS__))
-#define $reduce_32(op, a, ...) FIXBUG((a) op $reduce_31(op,  __VA_ARGS__))
+#define $reduce_4(op, a, ...) $fixbug((a) op $reduce_3(op, __VA_ARGS__))
+#define $reduce_5(op, a, ...) $fixbug((a) op $reduce_4(op, __VA_ARGS__))
+#define $reduce_6(op, a, ...) $fixbug((a) op $reduce_5(op, __VA_ARGS__))
+#define $reduce_7(op, a, ...) $fixbug((a) op $reduce_6(op,  __VA_ARGS__))
+#define $reduce_8(op, a, ...) $fixbug((a) op $reduce_7(op,  __VA_ARGS__))
+#define $reduce_9(op, a, ...) $fixbug((a) op $reduce_8(op,  __VA_ARGS__))
+#define $reduce_10(op, a, ...) $fixbug((a) op $reduce_9(op,  __VA_ARGS__))
+#define $reduce_11(op, a, ...) $fixbug((a) op $reduce_10(op,  __VA_ARGS__))
+#define $reduce_12(op, a, ...) $fixbug((a) op $reduce_11(op,  __VA_ARGS__))
+#define $reduce_13(op, a, ...) $fixbug((a) op $reduce_12(op,  __VA_ARGS__))
+#define $reduce_14(op, a, ...) $fixbug((a) op $reduce_13(op,  __VA_ARGS__))
+#define $reduce_15(op, a, ...) $fixbug((a) op $reduce_14(op,  __VA_ARGS__))
+#define $reduce_16(op, a, ...) $fixbug((a) op $reduce_15(op,  __VA_ARGS__))
+#define $reduce_17(op, a, ...) $fixbug((a) op $reduce_16(op,  __VA_ARGS__))
+#define $reduce_18(op, a, ...) $fixbug((a) op $reduce_17(op,  __VA_ARGS__))
+#define $reduce_19(op, a, ...) $fixbug((a) op $reduce_18(op,  __VA_ARGS__))
+#define $reduce_20(op, a, ...) $fixbug((a) op $reduce_19(op,  __VA_ARGS__))
+#define $reduce_21(op, a, ...) $fixbug((a) op $reduce_20(op,  __VA_ARGS__))
+#define $reduce_22(op, a, ...) $fixbug((a) op $reduce_21(op,  __VA_ARGS__))
+#define $reduce_23(op, a, ...) $fixbug((a) op $reduce_22(op,  __VA_ARGS__))
+#define $reduce_24(op, a, ...) $fixbug((a) op $reduce_23(op,  __VA_ARGS__))
+#define $reduce_25(op, a, ...) $fixbug((a) op $reduce_24(op,  __VA_ARGS__))
+#define $reduce_26(op, a, ...) $fixbug((a) op $reduce_25(op,  __VA_ARGS__))
+#define $reduce_27(op, a, ...) $fixbug((a) op $reduce_26(op,  __VA_ARGS__))
+#define $reduce_28(op, a, ...) $fixbug((a) op $reduce_27(op,  __VA_ARGS__))
+#define $reduce_29(op, a, ...) $fixbug((a) op $reduce_28(op,  __VA_ARGS__))
+#define $reduce_30(op, a, ...) $fixbug((a) op $reduce_29(op,  __VA_ARGS__))
+#define $reduce_31(op, a, ...) $fixbug((a) op $reduce_30(op,  __VA_ARGS__))
+#define $reduce_32(op, a, ...) $fixbug((a) op $reduce_31(op,  __VA_ARGS__))
 //...
 #define $reduce(...) $overload($reduce_, __VA_ARGS__)
 
@@ -135,35 +135,35 @@
 #define $map_1(func) 
 #define $map_2(func, a)      (func(a))
 #define $map_3(func, a, b)   (func(a)), (func(b))
-#define $map_4(func, a, ...) (func(a)), FIXBUG($map_3(func, __VA_ARGS__))
-#define $map_5(func, a, ...) (func(a)), FIXBUG($map_4(func, __VA_ARGS__))
-#define $map_6(func, a, ...) (func(a)), FIXBUG($map_5(func, __VA_ARGS__))
-#define $map_7(func, a, ...) (func(a)), FIXBUG($map_6(func, __VA_ARGS__))
-#define $map_8(func, a, ...) (func(a)), FIXBUG($map_7(func, __VA_ARGS__))
-#define $map_9(func, a, ...) (func(a)), FIXBUG($map_8(func, __VA_ARGS__))
-#define $map_10(func, a, ...) (func(a)), FIXBUG($map_9(func, __VA_ARGS__))
-#define $map_11(func, a, ...) (func(a)), FIXBUG($map_10(func, __VA_ARGS__))
-#define $map_12(func, a, ...) (func(a)), FIXBUG($map_11(func, __VA_ARGS__))
-#define $map_13(func, a, ...) (func(a)), FIXBUG($map_12(func, __VA_ARGS__))
-#define $map_14(func, a, ...) (func(a)), FIXBUG($map_13(func, __VA_ARGS__))
-#define $map_15(func, a, ...) (func(a)), FIXBUG($map_14(func, __VA_ARGS__))
-#define $map_16(func, a, ...) (func(a)), FIXBUG($map_15(func, __VA_ARGS__))
-#define $map_17(func, a, ...) (func(a)), FIXBUG($map_16(func, __VA_ARGS__))
-#define $map_18(func, a, ...) (func(a)), FIXBUG($map_17(func, __VA_ARGS__))
-#define $map_19(func, a, ...) (func(a)), FIXBUG($map_18(func, __VA_ARGS__))
-#define $map_20(func, a, ...) (func(a)), FIXBUG($map_19(func, __VA_ARGS__))
-#define $map_21(func, a, ...) (func(a)), FIXBUG($map_20(func, __VA_ARGS__))
-#define $map_22(func, a, ...) (func(a)), FIXBUG($map_21(func, __VA_ARGS__))
-#define $map_23(func, a, ...) (func(a)), FIXBUG($map_22(func, __VA_ARGS__))
-#define $map_24(func, a, ...) (func(a)), FIXBUG($map_23(func, __VA_ARGS__))
-#define $map_25(func, a, ...) (func(a)), FIXBUG($map_24(func, __VA_ARGS__))
-#define $map_26(func, a, ...) (func(a)), FIXBUG($map_25(func, __VA_ARGS__))
-#define $map_27(func, a, ...) (func(a)), FIXBUG($map_26(func, __VA_ARGS__))
-#define $map_28(func, a, ...) (func(a)), FIXBUG($map_27(func, __VA_ARGS__))
-#define $map_29(func, a, ...) (func(a)), FIXBUG($map_28(func, __VA_ARGS__))
-#define $map_30(func, a, ...) (func(a)), FIXBUG($map_29(func, __VA_ARGS__))
-#define $map_31(func, a, ...) (func(a)), FIXBUG($map_30(func, __VA_ARGS__))
-#define $map_32(func, a, ...) (func(a)), FIXBUG($map_31(func, __VA_ARGS__))
+#define $map_4(func, a, ...) (func(a)), $fixbug($map_3(func, __VA_ARGS__))
+#define $map_5(func, a, ...) (func(a)), $fixbug($map_4(func, __VA_ARGS__))
+#define $map_6(func, a, ...) (func(a)), $fixbug($map_5(func, __VA_ARGS__))
+#define $map_7(func, a, ...) (func(a)), $fixbug($map_6(func, __VA_ARGS__))
+#define $map_8(func, a, ...) (func(a)), $fixbug($map_7(func, __VA_ARGS__))
+#define $map_9(func, a, ...) (func(a)), $fixbug($map_8(func, __VA_ARGS__))
+#define $map_10(func, a, ...) (func(a)), $fixbug($map_9(func, __VA_ARGS__))
+#define $map_11(func, a, ...) (func(a)), $fixbug($map_10(func, __VA_ARGS__))
+#define $map_12(func, a, ...) (func(a)), $fixbug($map_11(func, __VA_ARGS__))
+#define $map_13(func, a, ...) (func(a)), $fixbug($map_12(func, __VA_ARGS__))
+#define $map_14(func, a, ...) (func(a)), $fixbug($map_13(func, __VA_ARGS__))
+#define $map_15(func, a, ...) (func(a)), $fixbug($map_14(func, __VA_ARGS__))
+#define $map_16(func, a, ...) (func(a)), $fixbug($map_15(func, __VA_ARGS__))
+#define $map_17(func, a, ...) (func(a)), $fixbug($map_16(func, __VA_ARGS__))
+#define $map_18(func, a, ...) (func(a)), $fixbug($map_17(func, __VA_ARGS__))
+#define $map_19(func, a, ...) (func(a)), $fixbug($map_18(func, __VA_ARGS__))
+#define $map_20(func, a, ...) (func(a)), $fixbug($map_19(func, __VA_ARGS__))
+#define $map_21(func, a, ...) (func(a)), $fixbug($map_20(func, __VA_ARGS__))
+#define $map_22(func, a, ...) (func(a)), $fixbug($map_21(func, __VA_ARGS__))
+#define $map_23(func, a, ...) (func(a)), $fixbug($map_22(func, __VA_ARGS__))
+#define $map_24(func, a, ...) (func(a)), $fixbug($map_23(func, __VA_ARGS__))
+#define $map_25(func, a, ...) (func(a)), $fixbug($map_24(func, __VA_ARGS__))
+#define $map_26(func, a, ...) (func(a)), $fixbug($map_25(func, __VA_ARGS__))
+#define $map_27(func, a, ...) (func(a)), $fixbug($map_26(func, __VA_ARGS__))
+#define $map_28(func, a, ...) (func(a)), $fixbug($map_27(func, __VA_ARGS__))
+#define $map_29(func, a, ...) (func(a)), $fixbug($map_28(func, __VA_ARGS__))
+#define $map_30(func, a, ...) (func(a)), $fixbug($map_29(func, __VA_ARGS__))
+#define $map_31(func, a, ...) (func(a)), $fixbug($map_30(func, __VA_ARGS__))
+#define $map_32(func, a, ...) (func(a)), $fixbug($map_31(func, __VA_ARGS__))
 //...
 #define $map(...) $overload($map_, __VA_ARGS__)
 
